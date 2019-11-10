d8ba5effe7d6b4cadc20a7d5fef11be9a63ffd0c
XamVolo <xamvolo@xamvolos-imac.home>
2019-11-10 21:28:39 +0000

    write test for divisible by neither 3 nor 5 - return self

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    'fizz'
+  if num % 3 == 0 && num % 5 == 0
+    'fizzbuzz'
   elsif num % 5 == 0
     'buzz'
-  else
-    nil
+  elsif num % 3 == 0
+    'fizz'
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,7 @@
 require "fizzbuzz"
 
 RSpec.describe "fizzbuzz" do
+
   context "when number is divisible by 3" do
     it "should return 'fizz'" do
       expect(fizzbuzz(3)).to eq("fizz")
@@ -8,6 +9,7 @@ RSpec.describe "fizzbuzz" do
       expect(fizzbuzz(10)).not_to eq("fizz")
     end
   end
+
   context "when number is divisible by 5" do
     it "should return 'buzz'" do
       expect(fizzbuzz(5)).to eq("buzz")
@@ -17,11 +19,22 @@ RSpec.describe "fizzbuzz" do
     end
   end
   context "when number is divisible by both" do
+    let(:num) { rand(1000) * 15 }
+    
     it "should return 'fizzbuzz'" do
       expect(fizzbuzz(15)).to eq("fizzbuzz")
       expect(fizzbuzz(45)).to eq("fizzbuzz")
       expect(fizzbuzz(80)).not_to eq("fizzbuzz")
       expect(fizzbuzz(27)).not_to eq("fizzbuzz")
+      expect(fizzbuzz(num)).to eq("fizzbuzz")
+    end
+  end
+
+  context "when number is neither divisible by 3 nor 5" do
+    it "should return the number" do
+      expect(fizzbuzz(13)).to eq(13)
+      expect(fizzbuzz(29)).to eq(29)
+      expect(fizzbuzz(144)).not_to eq(144)
     end
   end
 end
